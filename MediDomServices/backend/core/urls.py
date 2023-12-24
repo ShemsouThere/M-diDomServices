3d504a6a55791383ae93f57306e26857943b2993
Darkyy69 <radiasweetgirl812@gmail.com>
2023-12-24 18:41:44 +0100

    --FIXED

--- MediDomServices/backend/core/urls.py
+++ MediDomServices/backend/core/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.api.urls')),
-    # path('', TemplateView.as_view(template_name="base.html")),
+    path('', TemplateView.as_view(template_name="base.html")),
 ]
