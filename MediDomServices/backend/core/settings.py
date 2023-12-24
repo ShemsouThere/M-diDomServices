6a439fac4073dbec580f7cb1ca8f8f955fc55164
Darkyy69 <radiasweetgirl812@gmail.com>
2023-12-24 18:54:13 +0100

    WORKING... at port 8000

--- MediDomServices/backend/core/settings.py
+++ MediDomServices/backend/core/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-parent_directory = os.path.dirname(BASE_DIR)
+PROJECT_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [PROJECT_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,10 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "public",
-#     BASE_DIR / "src/assets",
-# ]
+STATICFILES_DIRS = [
+    PROJECT_DIR / "public",
+    PROJECT_DIR / "src/assets",
+]
 
 STATIC_ROOT = BASE_DIR / "static"
 
