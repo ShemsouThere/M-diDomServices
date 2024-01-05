09dfd89acbd6f5eb53fe1ae54cd19ea40f407b81
Darkyy69 <radiasweetgirl812@gmail.com>
2024-01-06 00:54:18 +0100

    Added Status for the "Consultation". Modified Consultation.jsx

--- MediDomServices/backend/client/models.py
+++ MediDomServices/backend/client/models.py
@@ -6,12 +6,19 @@ TYPE_CONSULTATION = [
     ('paramedicale', 'paramedicale')
 ]
 
+STATUS = [
+    ('En attente', 'En attente'),
+    ('Accepté', 'Accepté'),
+    ('Refusée', 'Refusée'),
+]
+
 class Consultation(models.Model):
 
     User = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     Date = models.DateField()
     Heure = models.TimeField()
     Type = models.CharField(choices=TYPE_CONSULTATION, max_length=15)
+    Status = models.CharField(choices=STATUS, max_length=12, default='En attente')
     
     def __str__(self):
-        return f'{self.User} - {self.Date} - {self.Heure} - {self.Type}'
+        return f'{self.User} - {self.Date} - {self.Heure} - {self.Type} - {self.Status}'
