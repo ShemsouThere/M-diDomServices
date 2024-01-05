f76fb57ceb075d09c17fecfe4d28f8ca8ddd8c78
Darkyy69 <radiasweetgirl812@gmail.com>
2024-01-05 23:56:28 +0100

    Consultations fetching done.

--- MediDomServices/src/component/Consultations.jsx
+++ MediDomServices/src/component/Consultations.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import { useState , React, useEffect} from "react";
+import axios from "axios";
+
 
 const Consultations = ({ isAuthenticated ,userRole}) => {
+
+    const [consultations, setConsultations] = useState([]);
+
+    const getConsultations = async () => {
+        try {
+
+            const response = await axios.get('/api/consultation/');
+            const data = response.data;
+            console.log(data);
+            setConsultations(data);
+
+        } catch (error) {
+            console.error('Error getting consultations:', error);
+        }
+    };
+    
+useEffect(() => {
+        getConsultations();
+}, []);
+
 return (
 <>
 <section className='Consultations'>
-<h1>Consultations Page</h1>
+<h2>Consultations Page</h2>
 
 {isAuthenticated && userRole==='client'&&
 (
-
+<>
         
-        <h1>Interface Client</h1>
+        {/* Display consultations in rows */}
+        {consultations.map((consultation, index) => (
+              <div key={index}>
+                <h4>Consultation N°: {index+1}</h4>
+                <h4>Heure: {consultation.Heure}</h4>
+                <h4>Date: {consultation.Date}</h4>
+                <h4>Type: {consultation.Type}</h4>
+              </div>
+            ))}
         
-
+</>
 )}
 
 {isAuthenticated && userRole==='responsable'
